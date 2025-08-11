0177bba73a99acf2e472045a05819293e6ff06f1
Priyansu159 <patelpriyansu159@gmail.com>
2025-08-11 21:26:36 +0530

    please

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Chat-App/', // 👈 must match your repo name exactly
+  base: '/Chat-App/', // 👈 must match your GitHub repo name exactly
 })
+
