4ac898f9319ad3fd408f5710f53f469a58079e3d
Priyansu159 <patelpriyansu159@gmail.com>
2025-08-23 21:40:01 +0530

    login error solved

--- src/lib/upload.js
+++ src/lib/upload.js
@@ -1,41 +1,30 @@
 import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
 import { storage } from "./firebase";
 
-const upload = async (file) =>{
+const upload = async (file) => {
+  if (!file) throw new Error("No file provided for upload");
 
-    const date = new Date()
+  const date = new Date();
+  const storageRef = ref(storage, `images/${date.getTime()}_${file.name}`);
+  const uploadTask = uploadBytesResumable(storageRef, file);
 
-
-    const storageRef = ref(storage, `images/${date.getTime()}_${file.name}`);
-const uploadTask = uploadBytesResumable(storageRef, file);
-
-return new Promise((resolve, reject) => {
-
-
-
-uploadTask.on('state_changed',
-  (snapshot) => {
-    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-    console.log('Upload is ' + progress + '% done');
-  }, 
-  (error) => {
-    reject("Something went wrong!" +error.code)
-    switch (error.code) {
-      case 'storage/unauthorized':
-        break;
-      case 'storage/canceled':
-        break;
-        case 'storage/unknown':
-          break;
+  return new Promise((resolve, reject) => {
+    uploadTask.on(
+      'state_changed',
+      snapshot => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log('Upload is ' + progress + '% done');
+      },
+      error => {
+        reject(new Error("Something went wrong! " + error.code));
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then(downloadURL => {
+          resolve(downloadURL);
+        });
       }
-    }, 
-    () => {
-      getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-        resolve(downloadURL)
-      });
-    }
-  );
-});
+    );
+  });
 };
 
 export default upload;
--- src/lib/userStore.js
+++ src/lib/userStore.js
@@ -6,20 +6,22 @@ export const useUserStore = create((set) => ({
   currentUser: null,
   isLoading: true,
   fetchUserInfo: async (uid) => {
-    if (!uid)  return set({currentUser : null, isLoading : false});
-    try {
-        const docRef = doc(db, "user", uid);
-        const docSnap = await getDoc (docRef);
-
-if (docSnap.exists()) {
-  set({currentUser:docSnap.data(),isLoading:false})
-}else[
-    set({currentUser : null, isLoading : false})
-]
-        
-    } catch (err) {
-        console.log(err);
-        return set({currentUser : null, isLoading : false});
+    if (!uid) {
+      set({ currentUser: null, isLoading: false });
+      return;
     }
-  }
-}))
+    set({ isLoading: true }); // Set loading at fetch start
+    try {
+      const docRef = doc(db, "user", uid);
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        set({ currentUser: docSnap.data(), isLoading: false });
+      } else {
+        set({ currentUser: null, isLoading: false });
+      }
+    } catch (err) {
+      console.log(err);
+      set({ currentUser: null, isLoading: false });
+    }
+  },
+}));
